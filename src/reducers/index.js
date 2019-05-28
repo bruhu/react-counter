338d041711f65982a615cb6c3bf8b7784972bbfe
Bru <annabru.abh@gmail.com>
2019-05-28 13:44:32 +0200

    adding reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,13 @@
+//THIS WILL BE OUR REDUCER
+
+//everything uppercase is temporary
+
+const reducer = (INITIAL, action) => {
+  switch (key) {
+    case value:
+      break;
+
+    default:
+      break;
+  }
+};
