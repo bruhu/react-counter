025411c2487629d6bc50fe36209aacf0269c273e
Bru <annabru.abh@gmail.com>
2019-05-28 14:07:43 +0200

    Store provided, error is gone

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <div className="App-intro">
-          <Provider>
+          <Provider store={store}>
             <Counter />
           </Provider>
           {/* <h2>Counter and Toggle - Update</h2>
