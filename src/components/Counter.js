164d9451f4ca6b8fc43df4733b03e57c7b6ac136
Bru <annabru.abh@gmail.com>
2019-05-28 13:42:07 +0200

    Created reducers folder w/index.js

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
 
 export default class Counter extends Component {
+    increment = () => {
+        console.log("Want tos inc")
+    }
+
+    decrement = () => {
+        console.log("Want to decrease counter")
+    }
   render() {
     return (
       <div>
         <h2>Counter and Toggle - Update</h2>
-        <button onClick="">+</button>
-        <button onClick="">-</button>
-        <div />
+        <button onClick={this.increment}>+</button>
+        <button onClick={this.decrement}>-</button>
+        <div>I'm a number</div>
       </div>
     );
   }
