85a8deed6de1d95ea6fccaea0dafa6c58e15387b
Bru <annabru.abh@gmail.com>
2019-05-28 15:36:01 +0200

    Date works.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-export default class Footer extends Component {
-    render() {
-        return (
-            <div>
-                Footer.
-            </div>
-        )
-    }
+class Footer extends Component {
+  render() {
+    return <div>Last update: {this.props.date ? this.props.date.toString() : "No updates yet"}</div>;
+  }
 }
+
+const mapStateToProps = state => {
+  return {
+    date: state.date
+  };
+};
+
+export default connect(mapStateToProps)(Footer);
